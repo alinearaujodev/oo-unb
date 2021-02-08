6376df71dd987d8880d577d39d2fbffdbba72c9b
Aline Araújo <alineprofissional@outlook.com>
2021-02-08 10:25:03 -0300

    hello world em java

---
+++ src/HelloWorld.java
@@ -0,0 +1,10 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!"); // println é para saltar uma linha 
+		System.out.print("@alinearaujodev");
+
+	}
+
+}
