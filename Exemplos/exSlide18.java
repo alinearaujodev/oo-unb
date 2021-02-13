4a76f7b017b1bf26960f9576ccc01bf960a041c1
Aline Araújo <alineprofissional@outlook.com>
2021-02-12 21:03:19 -0300

    Ex Slide 18

---
+++ Exemplos/exSlide18.java
@@ -0,0 +1,15 @@
+
+public class exSlide18 {
+
+	public static void main(String[] args) {
+		double alt1 = 1.58;
+		double alt2 = 2.07;
+		double alt3 = 0.55;
+		
+		double media = (alt1 + alt2 + alt3)/3;
+		
+		System.out.println(media);
+
+	}
+
+}
