494289973102332321815700364b872ac009006b
Aline Araújo <alineprofissional@outlook.com>
2021-02-14 17:29:53 -0300

    versao final incompleta

--- TP01/src/inmte.java
+++ TP01/src/inmte.java
@@ -117,10 +117,8 @@ public class inmte {
 	
 	static void calcularMinima (int x, int y, double z[]) {
 		// FUNCAO PARA CALCULAR A temperatura minima
-		int c;
-		int[] diaMin = new int[31];
+		int c, diaMin = 1;
 		double minima = 10000, min = 0;
-		//boolean repetiu = false;
 				
 		System.out.print("-------------------------\n");
 		// MÊS COM 31 DIAS
@@ -129,134 +127,172 @@ public class inmte {
 				if (z[c] < minima) {
 					min = z[c];
 					minima = min;
-					//diaMin = (c + 1);
+					diaMin = (c + 1);
 				}
-			}	
+			}
+			System.out.print("Temperatura Mínima = " + min + "\n");
+			System.out.print(diaMin + "/" + x + "/" + y + "\n");
+			
 		} // MÊS COM 30 DIAS
 		else if (x == 4 || x == 6 || x == 9 || x == 11) {
 			for (c = 0; c <=29; c++) {
 				if (z[c] < minima) {
 					min = z[c];
 					minima = min;
-					//diaMin = (c + 1);
+					diaMin = (c + 1);
 				}
-			}	
+			}
+			System.out.print("Temperatura Mínima = " + min + "\n");
+			System.out.print(diaMin + "/" + x + "/" + y + "\n");
+			
 		} // MÊS de FEVEREIRO COM 29 DIAS
 		else if (x == 2 && (y == 2012 || y == 2016 || y == 2020)) {
 			for (c = 0; c <=28; c++) {
 				if (z[c] < minima) {
 					min = z[c];
 					minima = min;
-					//diaMin = (c + 1);
+					diaMin = (c + 1);
 				}
 			}	
+			System.out.print("Temperatura Mínima = " + min + "\n");
+			System.out.print(diaMin + "/" + x + "/" + y + "\n");
+			
 		} // MÊS de FEVEREIRO COM 28 DIAS
 		else if (x == 2 && (y != 2012 || y != 2016 || y != 2020)) {
 			for (c = 0; c <=27; c++) {
 				if (z[c] < minima) {
 					min = z[c];
 					minima = min;
-					diaMin[c] = (c +1); 
-					//for(j = c+1; j < 27; j++){
-					//	if (z[j] == z[c]) {
-						//  repetiu = true;
-					//	}
-					//}
+					diaMin = (c +1); 
 				}
-
 			}	
 			System.out.print("Temperatura Mínima = " + min + "\n");
-			for (c = 0; c <=27; c++) {
-				System.out.print(diaMin[c] + "/" + x + "/" + y + "\n");
-			}
+			System.out.print(diaMin + "/" + x + "/" + y + "\n");
 		} 
 	}
 
 	static void calcularMaxima(int x, int y, double z[]) {
 		// FUNCAO PARA CALCULAR A temperatura maxima
 		int c,  diaMax = 1;
-		double maxima = 0, max = 0;
+		double maxima = 0, max = 1;
 						
 		System.out.print("-------------------------\n");
-		for (c=0; c< z.length; c++) {
-			if (z[c] > maxima) {
-				max = z[c];
-				maxima = max;
-				diaMax = (c + 1);
+		// MÊS COM 31 DIAS
+		if (x == 1 || x == 3 || x == 5 || x == 7 || x == 8 || x == 10 || x == 12) {
+			for (c = 0; c <=30; c++) {
+				if (z[c] > maxima) {
+					max = z[c];
+					maxima = max;
+					diaMax = (c + 1);
+				}
 			}
-		}
-						
-		System.out.print("Temperatura Maxima = " + max + "\n");
-		System.out.print(diaMax + "/" + x + "/" + y + "\n");
+			System.out.print("Temperatura Maxima = " + max + "\n");
+			System.out.print(diaMax + "/" + x + "/" + y + "\n");
+					
+		} // MÊS COM 30 DIAS
+		else if (x == 4 || x == 6 || x == 9 || x == 11) {
+			for (c = 0; c <=29; c++) {
+				if (z[c] > maxima) {
+					max = z[c];
+					maxima = max;
+					diaMax = (c + 1);
+				}
+			}
+			System.out.print("Temperatura Maxima = " + max + "\n");
+			System.out.print(diaMax + "/" + x + "/" + y + "\n");
+					
+		} // MÊS de FEVEREIRO COM 29 DIAS
+		else if (x == 2 && (y == 2012 || y == 2016 || y == 2020)) {
+			for (c = 0; c <=28; c++) {
+				if (z[c] > maxima) {
+					max = z[c];
+					maxima = max;
+					diaMax = (c + 1);
+				}
+			}	
+			System.out.print("Temperatura Maxima = " + max + "\n");
+			System.out.print(diaMax + "/" + x + "/" + y + "\n");
+			
+		} // MÊS de FEVEREIRO COM 28 DIAS
+		else if (x == 2 && (y != 2012 || y != 2016 || y != 2020)) {
+			for (c = 0; c <=27; c++) {
+				if (z[c] > maxima) {
+					max = z[c];
+					maxima = max;
+					diaMax = (c+1); 
+				}
+			}	
+			System.out.print("Temperatura Maxima = " + max + "\n");
+			System.out.print(diaMax + "/" + x + "/" + y + "\n");
+		} 
 		
 	}
 	
 	static void printDia(int x, int y, double z[]) {
 		int c;
 		System.out.print("-------------------------\n");
-		for (c=0; c< z.length; c++) {
-			System.out.print("Dia " + (c+1) + ": " + z[c] + "\n");
-		}
+		// MÊS COM 31 DIAS
+		if (x == 1 || x == 3 || x == 5 || x == 7 || x == 8 || x == 10 || x == 12) {
+			for (c = 0; c <=30; c++) {
+				System.out.print("Dia " + (c+1) + ": " + z[c] + "\n");
+			}
+		}// MÊS COM 30 DIAS
+		else if (x == 4 || x == 6 || x == 9 || x == 11) {
+			for (c = 0; c <=29; c++) {
+				System.out.print("Dia " + (c+1) + ": " + z[c] + "\n");
+			}
+							
+		} // MÊS de FEVEREIRO COM 29 DIAS
+		else if (x == 2 && (y == 2012 || y == 2016 || y == 2020)) {
+			for (c = 0; c <=28; c++) {
+				System.out.print("Dia " + (c+1) + ": " + z[c] + "\n");
+			}
+		} // MÊS de FEVEREIRO COM 28 DIAS
+		else if (x == 2 && (y != 2012 || y != 2016 || y != 2020)) {
+			for (c = 0; c <=27; c++) {
+				System.out.print("Dia " + (c+1) + ": " + z[c] + "\n");
+			}	
+		} 
 	}
 
 	public static void main(String[] args) {
-		int opcao, mes = 1, ano = 2020, copMes = mes, copAno = ano, escolheMes, escolheAno;
+		int opcao, mes = 1, ano = 2020, copMes = mes, copAno = ano;
 		
 		Scanner ler = new Scanner(System.in); //LEITURA DOS DADOS
-		
-		// Ano de 2011
-		//double[] jan11 = new double[30], fev11 = new double[28], mar11 = new double[31], abr11 = new double[30], mai11 = new double[31], jun11 = new double[30], jul11 = new double[31], ago11 = new double[31], set11 = new double[30], out11 = new double[31], nov11 = new double[30], dez11 = new double[31];         
-	// Ano de 2012
-		//double[] jan12 = new double[30], fev12 = new double[29], mar12 = new double[31], abr12 = new double[30], mai12 = new double[31], jun12 = new double[30], jul12 = new double[31], ago12 = new double[31], set12 = new double[30], out12 = new double[31], nov12 = new double[30], dez12 = new double[31];         
-	// Ano de 2013
-		//double[] jan13 = new double[30], fev13 = new double[28], mar13 = new double[31], abr13 = new double[30], mai13 = new double[31], jun13 = new double[30], jul13 = new double[31], ago13 = new double[31], set13 = new double[30], out13 = new double[31], nov13 = new double[30], dez13 = new double[31];         
-	// Ano de 2014
-		//double[] jan14 = new double[30], fev14 = new double[28], mar14 = new double[31], abr14 = new double[30], mai14 = new double[31], jun14 = new double[30], jul14 = new double[31], ago14 = new double[31], set14 = new double[30], out14 = new double[31], nov14 = new double[30], dez14 = new double[31];         
-	// Ano de 2015
-		//double[] jan15 = new double[30], fev15 = new double[28], mar15 = new double[31], abr15 = new double[30], mai15 = new double[31], jun15 = new double[30], jul15 = new double[31], ago15 = new double[31], set15 = new double[30], out15 = new double[31], nov15 = new double[30], dez15 = new double[31];         
-	// Ano de 2016
-		//double[] jan16 = new double[30], fev16 = new double[29], mar16 = new double[31], abr16 = new double[30], mai16 = new double[31], jun16 = new double[30], jul16 = new double[31], ago16 = new double[31], set16 = new double[30], out16 = new double[31], nov16 = new double[30], dez16 = new double[31];         
-	// Ano de 2017
-		//double[] jan17 = new double[30], fev17 = new double[28], mar17 = new double[31], abr17 = new double[30], mai17 = new double[31], jun17 = new double[30], jul17 = new double[31], ago17 = new double[31], set17 = new double[30], out17 = new double[31], nov17 = new double[30], dez17 = new double[31];         
-	// Ano de 2018
-		//double[] jan18 = new double[30], fev18 = new double[28], mar18 = new double[31], abr18 = new double[30], mai18 = new double[31], jun18 = new double[30], jul18 = new double[31], ago18 = new double[31], set18 = new double[30], out18 = new double[31], nov18 = new double[30], dez18 = new double[31];         
-	// Ano de 2019
-		//double[] jan19 = new double[30], fev19 = new double[28], mar19 = new double[31], abr19 = new double[30], mai19 = new double[31], jun19 = new double[30], jul19 = new double[31], ago19 = new double[31], set19 = new double[30], out19 = new double[31], nov19 = new double[30], dez19 = new double[31];         
-	// Ano de 2020
-		double[] jan20 = new double[31], fev20 = new double[31];//, mar20 = new double[31], abr20 = new double[30], mai20 = new double[31], jun20 = new double[30], jul20 = new double[31], ago20 = new double[31], set20 = new double[30], out20 = new double[31], nov20 = new double[30], dez20 = new double[31];         
+		double[] mes1 = new double[31], mes1Cop = new double[31];
 	
 		
 		// Dados janeiro 2020
-		jan20 [0] = 32.5;
-		jan20 [1] = 30.2;
-		jan20 [2] = 32.7;
-		jan20 [3] = 32;
-		jan20 [4] = 23;
-		jan20 [5] = 26.5;
-		jan20 [6] = 21;
-		jan20 [7] = 20.5;
-		jan20 [8] = 20;
-		jan20 [9] = 19;
-		jan20 [10] = 25;
-		jan20 [11] = 22.5;
-		jan20 [12] = 24;
-		jan20 [13] = 32;
-		jan20 [14] = 25.8;
-		jan20 [15] = 22.3;
-		jan20 [16] = 21;
-		jan20 [17] = 27;
-		jan20 [18] = 28.5;
-		jan20 [19] = 21;
-		jan20 [20] = 22.5;
-		jan20 [21] = 23.7;
-		jan20 [22] = 30;
-		jan20 [23] = 31.5;
-		jan20 [24] = 31;
-		jan20 [25] = 27.5;
-		jan20 [26] = 25;
-		jan20 [27] = 32.5;
-		jan20 [28] = 32.5;
-		jan20 [29] = 32.5;	
+		mes1 [0] = 32.5;
+		mes1 [1] = 30.2;
+		mes1 [2] = 32.7;
+		mes1 [3] = 32;
+		mes1 [4] = 23;
+		mes1 [5] = 26.5;
+		mes1 [6] = 21;
+		mes1 [7] = 20.5;
+		mes1 [8] = 20;
+		mes1 [9] = 19;
+		mes1 [10] = 25;
+		mes1 [11] = 22.5;
+		mes1 [12] = 24;
+		mes1 [13] = 32;
+		mes1 [14] = 25.8;
+		mes1 [15] = 22.3;
+		mes1 [16] = 21;
+		mes1 [17] = 27;
+		mes1 [18] = 28.5;
+		mes1 [19] = 21;
+		mes1 [20] = 22.5;
+		mes1 [21] = 23.7;
+		mes1 [22] = 30;
+		mes1 [23] = 31.5;
+		mes1 [24] = 31;
+		mes1 [25] = 27.5;
+		mes1 [26] = 25;
+		mes1 [27] = 32.5;
+		mes1 [28] = 32.5;
+		mes1 [29] = 32.5;	
 		
 		do {
 			menu();
@@ -276,14 +312,14 @@ public class inmte {
 						System.out.print("Ano: ");
 						ano = ler.nextInt();	
 					} while (ano <=2010 || ano >= 2021);
-					saberDados(mes, ano, jan20);
+					saberDados(mes, ano, mes1);
 					break;	
 				case 2:
 					System.out.print("-------------------------\n");
 					System.out.print("Cálculo da temperatura média\n");
 					escolherMes(copMes);
 					escolherAno(copAno);
-					calcularMedia(copMes, copAno, fev20);
+					calcularMedia(copMes, copAno, mes1Cop);
 					break;
 					
 				case 3:
@@ -291,7 +327,7 @@ public class inmte {
 					System.out.print("Cálculo da temperatura mínima\n");
 					escolherMes(copMes);
 					escolherAno(copAno);
-					calcularMinima(copMes, copAno, fev20);
+					calcularMinima(copMes, copAno, mes1Cop);
 					break;
 					
 				case 4:
@@ -299,7 +335,7 @@ public class inmte {
 					System.out.print("Cálculo da temperatura máxima\n");
 					escolherMes(copMes);
 					escolherAno(copAno);
-					calcularMaxima(copMes, copAno, fev20);
+					calcularMaxima(copMes, copAno, mes1Cop);
 					break;
 					
 				case 5:
@@ -307,12 +343,11 @@ public class inmte {
 					System.out.print("Relatório meteorológico\n");
 					escolherMes(copMes);
 					escolherAno(copAno);
-					printDia(copMes, copAno, fev20);
-					calcularMedia(copMes, copAno, fev20);
-					calcularMinima(copMes, copAno, fev20);
-					calcularMaxima(copMes, copAno, fev20);
-					break;
-					
+					printDia(copMes, copAno, mes1Cop);
+					calcularMedia(copMes, copAno, mes1Cop);
+					calcularMinima(copMes, copAno, mes1Cop);
+					calcularMaxima(copMes, copAno, mes1Cop);
+					break;			
 				case 6:
 					// SAIR DO PROGRAMA
 					System.out.print("-------------------------\n");
@@ -327,8 +362,8 @@ public class inmte {
 			// Fazer variavel de escopo global
 			copMes = mes;
 			copAno = ano;
-			for (int i=0; i < jan20.length; i++) {
-				fev20[i]=jan20[i]; 
+			for (int i=0; i < mes1.length; i++) {
+				mes1Cop[i]=mes1[i]; 
 			}
 		} while (opcao != 6);
 
